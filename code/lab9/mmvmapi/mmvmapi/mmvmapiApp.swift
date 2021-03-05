61575272b60d58aa80b990dfc07da1592a1d37b9
Juan Pablo Arias Mora <jpmora@amazon.com>
2021-03-05 17:28:46 -0600

    Lab 9-10

---
+++ code/lab9/mmvmapi/mmvmapi/mmvmapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmvmapiApp.swift
+//  mmvmapi
+//
+//  Created by Juan Pablo Arias Mora on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct mmvmapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
